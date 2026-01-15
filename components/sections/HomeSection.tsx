6e14d1ce88fc398eed9a6d3de30b32234ef75e7f
AravindAiphant <aravind@aiphant.io>
2026-01-15 10:42:26 +0530

    whole next js conversion

--- components/sections/HomeSection.tsx
+++ components/sections/HomeSection.tsx
@@ -9,7 +9,7 @@ export default function HomeSection() {
             <h3 className="subtitle">Introduction</h3>
             <h3 className="title">UI/UX & Frontend Engineer</h3>
             <p className="desc">
-              I&apos;m a UI/UX Designer and Frontend Engineer who builds intuitive, scalable digital experiences across web, mobile, and AI-powered interfaces. I focus on clarity, usability, and clean implementation—turning complex ideas into products that feel simple and purposeful.
+              I&apos;m Aravind a UI/UX Designer and Frontend Engineer who builds intuitive, scalable digital experiences across web, mobile, and AI-powered interfaces. I focus on clarity, usability, and clean implementation—turning complex ideas into products that feel simple and purposeful.
             </p>
             <Image 
               src="/img/signature.png" 
